54416c761d5a4dbb2ba26d79b4d6fe3107eb21e1
frank02902 <ngwafrANKLIN29@gmail.com>
2024-04-25 05:28:02 +0100

    used router added babel

--- src/createAccount.js
+++ src/createAccount.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from './App.module.css';
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+
 
 const CreateAccount = (props) => {
 const navigate = useNavigate()
